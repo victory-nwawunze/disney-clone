d116a6c11cbfb986688f6a4e1c1fd5e685530b74
victory-nwawunze <victorynwawunze@gmail.com>
2022-03-31 09:30:27 -0700

    creating disney clone

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,12 +3,58 @@
 import React from "react";
 import styled from "styled-components";
 function Header() {
-  return <Container>Header</Container>;
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" />
+          <span>ORIGINALS</span>
+        </a>
+      </NavMenu>
+    </Nav>
+  );
 }
 
 export default Header;
 
-const Container = styled.div`
+const Nav = styled.nav`
   height: 70px;
   background-color: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 30px;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+  }
+  img {
+    height: 20px;
+  }
+  span {
+    font-size: 13px;
+    letter-spacing: 1.42px;
+  }
 `;
