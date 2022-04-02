b1a7f7ba5445a3e7283568eccdb5fc2e471809b1
victory-nwawunze <victorynwawunze@gmail.com>
2022-04-02 12:21:34 -0700

    updated detail page

--- src/component/Detail.js
+++ src/component/Detail.js
@@ -66,6 +66,7 @@ const ImageTitle = styled.div`
   width: 35vw;
   min-height: 170px;
   min-width: 200px;
+  margin-top: 60px;
   img {
     width: 100%;
     height: 100%;
@@ -129,4 +130,5 @@ const Description = styled.div`
   font-size: 20px;
   margin-top: 16px;
   color: rgb(249, 249, 249);
+  max-width: 760px;
 `;
