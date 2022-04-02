089fa4780badd805cb62706b4cd509739a0996a0
victory-nwawunze <victorynwawunze@gmail.com>
2022-04-02 12:13:52 -0700

    added login page

--- src/component/Login.js
+++ src/component/Login.js
@@ -8,6 +8,13 @@ function Login() {
       <CTA>
         <CTALogoOne src="/images/cta-logo-one.svg" />
         <SignUp>GET ALL THERE</SignUp>
+        <Description>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur
+          minus aperiam itaque eos animi iure fugiat illo nam vel distinctio
+          explicabo doloremque eligendi tempora in, ducimus excepturi
+          necessitatibus quia ex!
+        </Description>
+        <CTALogoTwo src="/images/cta-logo-two.png" />
       </CTA>
     </Container>
   );
@@ -19,7 +26,7 @@ const Container = styled.div`
   position: relative;
   height: calc(100vh - 70px);
   display: flex;
-  align-items: center;
+  align-items: top;
   justify-content: center;
   &:before {
     background-position: top;
@@ -32,6 +39,7 @@ const Container = styled.div`
     left: 0;
     right: 0;
     background-image: url("/images/login-background.jpg");
+    opacity: 0.7;
     z-index: -1;
   }
 `;
@@ -39,9 +47,11 @@ const Container = styled.div`
 const CTA = styled.div`
   max-width: 650px;
   padding: 80px 40px;
-  width: 80%;
+  width: 90%;
   display: flex;
   flex-direction: column;
+  margin-top: 100px;
+  align-items: center;
 `;
 const CTALogoOne = styled.img``;
 const SignUp = styled.a`
@@ -55,8 +65,22 @@ const SignUp = styled.a`
   font-size: 18px;
   cursor: pointer;
   transition: all 250ms;
+  letter-spacing: 1.5px;
+  margin-top: 8px;
+  margin-bottom: 12px;
 
   &:hover {
     background-color: #0483ee;
   }
+`;
+
+const Description = styled.p`
+  font-size: 11px;
+  letter-spacing: 1.5px;
+  text-align: center;
+  line-height: 1.5;
+`;
+
+const CTALogoTwo = styled.img`
+  width: 90%;
 `;
